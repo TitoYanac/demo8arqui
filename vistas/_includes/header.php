1a3cdbdd4fa60244188189d6e11b5877dcf0471a
unknown <titoyanac@gmail.com>
2021-01-08 21:46:42 -0500

    google tag analytic

--- vistas/_includes/header.php
+++ vistas/_includes/header.php
@@ -2,6 +2,15 @@
 <html class="no-js" lang="zxx">
 
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N6SDR7G');</script>
+<!-- End Google Tag Manager -->
+
+
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>ECPROJECT</title>
@@ -50,6 +59,11 @@
 </head>
 
 <body>
+
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6SDR7G"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!-- header-start -->
     <header>
         <div class="header-area ">
